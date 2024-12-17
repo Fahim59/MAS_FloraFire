aecf778556069516daa821589378be35a495686e
Fahim59 <mrahaman59@yahoo.com>
2024-12-17 17:47:29 +0600

    test case 11 added

---
+++ src/test/java/tests/Corner_Cases/TestCase_11.java
@@ -0,0 +1,148 @@
+/*
+ * Scenario 11: Package Upgrade – Addi. License Upgrade – Seasonal License New Buy
+*/
+
+package tests.Corner_Cases;
+
+import base.BaseClass;
+import org.testng.annotations.*;
+import pages.*;
+
+public class TestCase_11 extends BaseClass {
+    private PackageSelection_Page packageSelectionPage;
+    private LocationAndUser_Page locationAndUserPage;
+    private Payment_Page paymentPage;
+    private Receipt_Page receiptPage;
+
+    @BeforeMethod
+    public void initializePageObjects() {
+        packageSelectionPage = new PackageSelection_Page(driver);
+        locationAndUserPage = new LocationAndUser_Page(driver);
+        paymentPage = new Payment_Page(driver);
+        receiptPage = new Receipt_Page(driver);
+
+        upgradedPackageName = "Gold";
+
+        packagePrice = 14.0;                                    //Package Price
+        licenseCount = 12;                                     //Additional User Count
+
+        upgradedLicenseCount = 14;                           //Additional User Count Now
+
+        seasonalMonthTotalDays = 30;                       //(Month Days) Fixed
+        seasonalMonthUsedDays = 26;                       //Remaining Month Day
+
+        seasonalLicenseCount = 2;                       //Seasonal License Added
+        perUserSeasonalLicensePrice = 5;               //Seasonal License Price
+        seasonalMonth = 1;                            //Month
+
+        upgradedSeasonalLicenseCount = 2;           //New Seasonal License Added
+        upgradedPerUserSeasonalLicensePrice = 15;  //New Seasonal License Price
+        upgradedSeasonalMonth = 1;                //Month
+
+        promoDiscount = 15;
+    }
+
+    @Test(description = "Verify that the customer can upgrade package, additional and seasonal license and confirm the accuracy of prorated and recurring payment details and successfully submit the order.", priority = 1)
+    public void verifyCustomerPackageUpgradeAndLicensePurchase() throws InterruptedException {
+        /*
+         * calculating Prior Package Prepaid details
+         */
+
+        locationAndUserPage.clickLocationTab();
+
+        SmallWait(1000);
+
+        locationAndUserPage.calculatePriorPackagePrepaid();
+        locationAndUserPage.calculateSeasonalLicenseTotalFee_Prior();
+
+        /*
+         * package upgrade
+         */
+
+        packageSelectionPage.clickPackageTab();
+
+        upgradedPackagePrice = packageSelectionPage.selectPackageAndGetValue(upgradedPackageName);
+
+        Scroll_Down();
+        packageSelectionPage.clickSaveBtn();
+
+        logger.info("Customer upgraded package to: {} level", upgradedPackageName);
+
+        /*
+         * calculating Today's Package Change details
+        */
+
+        locationAndUserPage.clickLocationTab();
+
+        SmallWait(1000);
+
+        locationAndUserPage.calculateTodayPackageChange();
+        locationAndUserPage.calculateSeasonalLicenseTotalFee_Today();
+        logger.info("Seasonal License Total Price: {}", upgradedTotalAmount);
+
+        Object[] priceTable = locationAndUserPage.priceTable(upgradedLicenseCount);
+        totalLicensePrice = (double) priceTable[0];
+        perUserLicensePrice = (double) priceTable[1];
+
+        /*
+         * buying new additional license and seasonal license
+         */
+
+        locationAndUserPage.enterAdditionalLicense(upgradedLicenseCount);
+
+        locationAndUserPage.enterSeasonalLicenseAndMonth(upgradedSeasonalLicenseCount, upgradedSeasonalMonth);
+
+        Scroll_Down();
+        locationAndUserPage.clickSaveBtn();
+
+        /*
+         * verifying prorated and recurring order in payment page and submit order
+         */
+
+        logger.info("Customer upgraded package, buys additional and seasonal license successfully and clicked on save button.");
+
+        paymentPage.verifyProratedOrderTable();
+
+        paymentPage.verifyRecurringOrderTable(upgradedPackagePrice, upgradedLicenseCount);
+
+        paymentPage.clickTermsBtn();
+        Scroll_Down();
+
+        customerName = paymentPage.fetchNameValue();
+
+        paymentPage.clickSubmitOrderBtn();
+
+        logger.info("Customer verifies prorated and recurring order price and submit the order");
+    }
+
+    @Test(description = "Verify that after successful payment, the customer is successfully navigated to Receipt page", priority = 2)
+    public void verifyCustomerNavigationAfterPayment() throws InterruptedException {
+        SmallWait(1000);
+
+        verifyCurrentUrl(jsonData.getJSONObject("tabURL").getString("receipt"));
+
+        logger.info("Customer successfully navigated to the Receipt page");
+    }
+
+    @Test(description = "Verify that customer can see the receipt page check the prorated and recurring payment details", priority = 3)
+    public void verifyCustomerReceiptPageWithProratedAndRecurringOrderDetails() throws InterruptedException {
+        receiptPage.verifyProratedOrderTable();
+
+        Scroll_Down();
+
+        receiptPage.verifyRecurringOrderTable(upgradedPackagePrice, upgradedLicenseCount);
+
+        Scroll_Up();
+
+        logger.info("Customer viewed the receipt page and verified the prorated and recurring order details.");
+    }
+
+    @Test(description = "Verify that the customer has received the subscription upgrade receipt in email", priority = 4)
+    public void verifyCustomerReceivedSubscriptionUpgradeReceipt() throws InterruptedException {
+        SmallWait(60000);
+
+        checkReceipt("subscriptionUpgrade");
+
+        logger.info("Customer successfully received the Subscription Upgrade receipt.");
+    }
+}

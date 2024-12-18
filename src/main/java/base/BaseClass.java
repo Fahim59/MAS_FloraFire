eb26d722939cdd7c140f775c07c98fff84d7beb7
Fahim59 <mrahaman59@yahoo.com>
2024-12-18 11:22:39 +0600

    base class updated

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -5,19 +5,17 @@ import factory.DriverFactory;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+
 import org.apache.logging.log4j.*;
 import org.json.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.openqa.selenium.support.ui.*;
 import org.testng.Assert;
 import org.testng.annotations.*;
-import utils.ConfigLoader;
+import utils.*;
 
 import javax.activation.*;
 import javax.mail.*;
@@ -27,9 +25,7 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.io.*;
 import java.text.*;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.YearMonth;
+import java.time.*;
 import java.util.*;
 import java.util.List;
 

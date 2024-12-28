44815089cc9fc9457725a0ffc1e4244705613521
fahim59 <mrahaman59@yahoo.com>
2024-12-28 13:18:16 +0600

    base page updated

--- src/test/java/tests/Base.java
+++ src/test/java/tests/Base.java
@@ -7,16 +7,20 @@ import pages.*;
 public class Base extends BaseClass {
     private Home_Page homePage;
 
-    String userName, password;
-
     @BeforeMethod
     public void initializePageObjects() {
         homePage = new Home_Page(driver);
     }
 
-    @Test(description = "Verify that a user can ", priority = 1)
-    public void verifyUser() {
+    @Test(description = "Verify that after successful login, the customer is successfully navigated to ", priority = 1)
+    public void verifyCustomerNavigationAfterLogin() throws InterruptedException {
+        SmallWait(1000);
 
-        logger.info("User .");
+        //homePage.clickValueMenu();
+
+        SmallWait(1000);
+        //verifyCurrentUrl(jsonData.getJSONObject("tabURL").getString("valueTypes"));
+
+        logger.info("User clicked on the value menu and successfully navigated to the  page");
     }
 }

c2353033c21e94b0f536d766316c83ba4907c50c
fahim59 <mrahaman59@yahoo.com>
2024-12-28 13:14:44 +0600

    base page updated

--- src/test/java/tests/Base.java
+++ src/test/java/tests/Base.java
@@ -1,40 +1,22 @@
 package tests;
 
 import base.BaseClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-import pages.Home_Page;
-import pages.Login_Page;
+import org.testng.annotations.*;
+import pages.*;
 
 public class Base extends BaseClass {
-    private Login_Page loginPage;
     private Home_Page homePage;
 
     String userName, password;
 
     @BeforeMethod
     public void initializePageObjects() {
-        loginPage = new Login_Page(driver);
         homePage = new Home_Page(driver);
     }
 
-    @Test(description = "Verify that a user can log in successfully", priority = 1)
-    public void verifyUserSuccessfulLogin() {
-        Open_Website("");
+    @Test(description = "Verify that a user can ", priority = 1)
+    public void verifyUser() {
 
-        userName = jsonData.getJSONObject("login_info").getString("username"); //sean@qca6z4pm.mailosaur.net
-        password = jsonData.getJSONObject("login_info").getString("password");
-
-        loginPage.enterLoginDetails(userName, password);
-
-        logger.info("User logged in successfully.");
-    }
-
-    @Test(description = "Verify that after successful login, user can see the Home menu", priority = 2)
-    public void verifyHomeMenuVisibilityAfterLogin() throws InterruptedException {
-        SmallWait(1000);
-        homePage.verifyHomeMenuVisibility();
-
-        logger.info("User successfully navigated to the Home page");
+        logger.info("User .");
     }
 }

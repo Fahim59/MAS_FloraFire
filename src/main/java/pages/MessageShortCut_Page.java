a7a959fb8f9843488c21b7710770b3b36cee63dc
Fahim59 <mrahaman59@yahoo.com>
2024-12-31 18:12:28 +0600

    message shortcut page updated

---
+++ src/main/java/pages/MessageShortCut_Page.java
@@ -0,0 +1,80 @@
+package pages;
+
+import base.BaseClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class MessageShortCut_Page extends BaseClass{
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+    private final JavascriptExecutor js;
+    private final Actions actions;
+
+    public MessageShortCut_Page(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        js = (JavascriptExecutor) driver;
+        actions = new Actions(driver);
+    }
+
+    private final By newShortCutButton = By.xpath("//span[contains(text(),'New Message ShortCut')]");
+
+    private final By shortCodeField = By.xpath("//input[@formcontrolname='shortCut']");
+
+    private final By fullMessageField = By.xpath("//textarea[@formcontrolname='description']");
+
+    private final By saveBtn = By.xpath("//span[contains(text(),'Save')]");
+
+    private final By successMessage = By.xpath("//p[@class='abp-toast-message']");
+
+    public void clickNewShortCutButton() {
+        click_Element(newShortCutButton);
+    }
+
+    public MessageShortCut_Page enterShortCode(String code){
+        write_Send_Keys(shortCodeField, code);
+        return this;
+    }
+
+    public MessageShortCut_Page enterFullMessage(String message) throws InterruptedException {
+        SmallWait(500);
+        write_Send_Keys(fullMessageField, message);
+        return this;
+    }
+
+    public void clickSaveButton() throws InterruptedException {
+        SmallWait(500);
+        click_Element_Js(saveBtn);
+    }
+
+    public void enterShortCode(String code, String message) throws InterruptedException {
+        enterShortCode(code).enterFullMessage(message).clickSaveButton();
+    }
+
+    private final String shortcutTable = "//table[@role='table']/tbody";
+    private final By rows = By.xpath(shortcutTable+"/tr");
+
+    public String getSuccessMessage(){
+        return get_Text(successMessage);
+    }
+
+    public void verifyShortCodeAddition(String shortcut) {
+        for(int l = 1; l<= get_Size(rows); l++){
+
+            String code = driver.findElement(By.xpath(shortcutTable+ "/tr["+l+"]/td[2]")).getText();
+
+            if(code.equalsIgnoreCase(shortcut)){
+                logger.info("Short Code {} found", shortcut);
+                break;
+            }
+            else{
+                logger.info("Short Code {} not found", shortcut);
+            }
+        }
+    }
+}
---
+++ src/test/java/tests/Message_ShortCut.java
@@ -0,0 +1,64 @@
+package tests;
+
+import base.BaseClass;
+import base.DataSource;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pages.*;
+
+import java.util.Map;
+
+public class Message_ShortCut extends BaseClass {
+    private Home_Page homePage;
+    private MessageShortCut_Page messageShortCutPage;
+
+    @BeforeMethod
+    public void initializePageObjects() {
+        homePage = new Home_Page(driver);
+        messageShortCutPage = new MessageShortCut_Page(driver);
+    }
+
+    @Test(description = "Verify that after successful login, the customer is successfully navigated to Message Shortcut page", priority = 1)
+    public void verifyCustomerNavigationAfterLogin() throws InterruptedException {
+        SmallWait(1000);
+
+        homePage.clickMessageShortcutsMenu();
+
+        SmallWait(1000);
+        verifyCurrentUrl(jsonData.getJSONObject("tabURL").getString("messageShortcut"));
+
+        logger.info("User clicked on the Message Shortcut menu and successfully navigated to the Message Shortcut page");
+    }
+
+    public static String[] shortCutData(Map<String, String> valueData) {
+        /*
+         * data[0] = shortCode
+         * data[1] = message
+         */
+
+        String shortCode = valueData.get("Short Code");
+        String message = valueData.get("Message");
+
+        return new String[] {shortCode, message};
+    }
+
+    @Test(description = "Verify that the user can add shortcode data successfully", dataProvider = "excelData", dataProviderClass = DataSource.class, priority = 2)
+    @DataSource.SheetName("ShortCuts")
+    public void verifyShortCodeDataEntry(Map<String, String> data) throws InterruptedException {
+        String[] shortCodeInfo = shortCutData(data);
+
+        String message = jsonData.getJSONObject("successMessage").getString("shortCut");
+
+        messageShortCutPage.clickNewShortCutButton();
+
+        messageShortCutPage.enterShortCode(shortCodeInfo[0], shortCodeInfo[1]);
+
+        SmallWait(1000);
+
+        Assert.assertEquals(message, messageShortCutPage.getSuccessMessage());
+
+        messageShortCutPage.verifyShortCodeAddition(shortCodeInfo[0]);
+
+        logger.info("Short Code {} added successfully", shortCodeInfo[0]);
+    }
+}

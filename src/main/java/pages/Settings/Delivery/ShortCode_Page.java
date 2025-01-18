64c150b5ab8a75ca4f92bfb6d3012e8db28a08c7
fahim59 <mrahaman59@yahoo.com>
2025-01-18 22:22:33 +0600

    short code updated

--- src/main/java/pages/Settings/Delivery/ShortCode_Page.java
+++ src/main/java/pages/Settings/Delivery/ShortCode_Page.java
@@ -22,5 +22,197 @@ public class ShortCode_Page extends BaseClass{
         actions = new Actions(driver);
     }
 
-    private final By Field = By.xpath("");
+    private final By newCodeButton = By.xpath("//span[contains(text(),'Add New')]");
+
+    public void clickNewCodeButton() throws InterruptedException {
+        SmallWait(1000);
+        click_Element(newCodeButton);
+
+        SmallWait(1000);
+    }
+
+    /*
+     * Short Code
+     */
+
+    private final By codeField = By.xpath("//input[@formcontrolname='code']");
+
+    private final By locationNameField = By.xpath("//input[@formcontrolname='name']");
+
+    private final By addressField = By.xpath("//input[@formcontrolname='address1']");
+    private final By addressContField = By.xpath("//input[@formcontrolname='address2']");
+
+    private final By countryField = By.xpath("//input[@formcontrolname='countryId']");
+    private final By stateField = By.xpath("//input[@formcontrolname='stateProvinceId']");
+    private final By cityField = By.xpath("//input[@formcontrolname='city']");
+    private final By zipField = By.xpath("//input[@formcontrolname='zipCode']");
+
+    private final By phoneField = By.xpath("//input[@formcontrolname='phoneNumber']");
+
+    private final By specialInstructionField = By.xpath("//textarea[@formcontrolname='specialInstruction']");
+
+    private final By storeField = By.xpath("(//div[contains(@id,'mat-select-value')])[2]");
+    private final By deliveryTypeField = By.xpath("(//div[contains(@id,'mat-select-value')])[3]");
+    private final By deliveryZoneField = By.xpath("(//div[contains(@id,'mat-select-value')])[4]");
+
+    private final By defaultDeliveryChargeField = By.xpath("//input[@type='checkbox']");
+
+    private final By specialDeliveryChargeField = By.xpath("//input[@formcontrolname='specialDeliveryCharge']");
+
+    public ShortCode_Page enterShortCode(String code) {
+        write_Send_Keys(codeField, code);
+        return this;
+    }
+
+    public ShortCode_Page enterLocationName(String location) {
+        write_Send_Keys(locationNameField, location);
+        return this;
+    }
+
+    public ShortCode_Page enterAddress(String address){
+        write_Send_Keys(addressField, address);
+        return this;
+    }
+    public ShortCode_Page enterAddressCont(String addressCont){
+        write_Send_Keys(addressContField, addressCont);
+        return this;
+    }
+
+    public ShortCode_Page selectCountry(String country) throws InterruptedException {
+        click_Element(countryField);
+        //write_Send_Keys(countryField, country);
+
+        SmallWait(1500);
+        js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//span[contains(text(),'"+country+"')]")));
+        return this;
+    }
+    public ShortCode_Page selectState(String state) throws InterruptedException {
+        SmallWait(1000);
+        click_Element(stateField);
+        SmallWait(500);
+        write_Send_Keys(stateField, state);
+
+        SmallWait(500);
+        js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//span[contains(text(),'"+state+"')]")));
+        return this;
+    }
+
+    public ShortCode_Page enterCity(String city){
+        write_Send_Keys(cityField, city);
+        return this;
+    }
+    public ShortCode_Page enterZip(String zip){
+        write_Send_Keys(zipField, zip);
+        return this;
+    }
+
+    public ShortCode_Page enterPhone(String phone){
+        write_Send_Keys(phoneField, phone);
+        return this;
+    }
+
+    public ShortCode_Page enterSpecialInstruction(String instruction) {
+        write_Send_Keys(specialInstructionField, instruction);
+        return this;
+    }
+
+    public ShortCode_Page selectStore(String store) throws InterruptedException {
+        click_Element(storeField);
+
+        SmallWait(200);
+        js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//mat-option/span[contains(text(), '"+store+"')]")));
+
+        return this;
+    }
+
+    public ShortCode_Page selectDeliveryType(String type) throws InterruptedException {
+        click_Element(deliveryTypeField);
+
+        SmallWait(200);
+        js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//mat-option/span[contains(text(), '"+type+"')]")));
+
+        return this;
+    }
+
+    public ShortCode_Page selectDeliveryZone(String zone) throws InterruptedException {
+        click_Element(deliveryZoneField);
+
+        SmallWait(200);
+        js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//mat-option/span[contains(text(), '"+zone+"')]")));
+
+        return this;
+    }
+
+    public ShortCode_Page defaultDeliveryCharge(String flag) {
+        if(flag.equalsIgnoreCase("Yes")){
+            selectCheckBox(defaultDeliveryChargeField);
+        }
+        return this;
+    }
+
+    public void enterSpecialDeliveryCharge(String flag, String charge) {
+        if(flag.equalsIgnoreCase("No")){
+            write_Send_Keys(specialDeliveryChargeField, charge);
+        }
+    }
+
+    public void enterDeliveryShortCodeDetails(String code, String location, String address, String addressCont, String country,
+                                              String state, String city, String zip, String phone, String instruction, String store,
+                                              String type, String zone, String flag, String charge) throws InterruptedException {
+
+        enterShortCode(code).enterLocationName(location).enterAddress(address).enterAddressCont(addressCont).selectCountry(country).
+                selectState(state).enterCity(city).enterZip(zip).enterPhone(phone).enterSpecialInstruction(instruction).
+                selectStore(store).selectDeliveryType(type).selectDeliveryZone(zone).defaultDeliveryCharge(flag).
+                enterSpecialDeliveryCharge(flag, charge);
+    }
+
+    /*
+     * Button and Message
+     */
+
+    private final By validateBtn = By.xpath("//span[contains(text(),'Validate')]");
+
+    private final By saveBtn = By.xpath("(//span[contains(text(),'Save')])[1]");
+
+    private final By successMessage = By.xpath("//p[@class='abp-toast-message']");
+
+    private final By suggestionText = By.xpath("//div[@class='cdk-overlay-container']//li[1]");
+
+    public String getValidationMessage(){
+        return get_Text(successMessage);
+    }
+
+    public void clickValidateButton() throws InterruptedException {
+        SmallWait(1000);
+        click_Element(validateBtn);
+
+        SmallWait(2000);
+    }
+
+    public void clickSaveButton() throws InterruptedException {
+        click_Element_Js(saveBtn);
+        SmallWait(2000);
+    }
+
+    public void clickSuggestionText() throws InterruptedException {
+        SmallWait(1000);
+        click_Element(suggestionText);
+    }
+
+    private final String codeTable = "//table[@role='table']/tbody";
+    private final By rows = By.xpath(codeTable+"/tr");
+
+    public void verifyDeliveryShortCodeAddition(String code) throws InterruptedException {
+        SmallWait(1500);
+
+        for(int l = 1; l<= get_Size(rows); l++){
+
+            String deliveryCode = driver.findElement(By.xpath(codeTable+ "/tr["+l+"]/td[2]")).getText();
+
+            if(deliveryCode.equalsIgnoreCase(code)){
+                logger.info("Delivery Code {} found", code);
+                break;
+            }
+        }
+    }
 }
--- src/test/java/tests/Settings/Delivery/Short_Code.java
+++ src/test/java/tests/Settings/Delivery/Short_Code.java
@@ -32,29 +32,77 @@ public class Short_Code extends BaseClass {
 
     public static String[] shortCodeData(Map<String, String> valueData) {
         /*
-         * data[0] = flag
-         * data[1] = id
-         * data[2] = name
-         * data[3] = address
-         * data[4] = address cont
-         * data[5] = country
+         * data[0] = code
+         * data[1] = location
+         * data[2] = address
+         * data[3] = address cont
+         * data[4] = country
+         * data[5] = state
+         * data[6] = city
+         * data[7] = zip
+         * data[8] = phone
+         * data[9] = instruction
+         * data[10] = type
+         * data[11] = zone
+         * data[12] = flag
+         * data[13] = charge
          */
 
-        String flag = valueData.get("Flag");
-        String id = valueData.get("Id");
-        String name = valueData.get("Name");
+        String code = valueData.get("Short Code");
+        String location = valueData.get("Location");
         String address = valueData.get("Address");
         String address_cont = valueData.get("Address Cont.");
+        String country = valueData.get("Country");
+        String state = valueData.get("State");
+        String city = valueData.get("City");
+        String zip = valueData.get("Zip");
+        String phone = valueData.get("Phone");
+        String instruction = valueData.get("Special Instruction");
+        String type = valueData.get("Delivery Type");
+        String zone = valueData.get("Delivery Zone");
+        String flag = valueData.get("Flag");
+        String charge = valueData.get("Special Delivery Charge");
 
-        return new String[] {flag, id, name, address, address_cont};
+        return new String[] {code, location, address, address_cont, country, state, city, zip, phone,
+                instruction, type, zone, flag, charge};
     }
 
     @Test(description = "Verify that the user can add Short Code data successfully", dataProvider = "excelData", dataProviderClass = DataSource.class, priority = 2)
-    @DataSource.SheetName("Test")
+    @DataSource.SheetName("ShortCode")
     public void verifyShortCodeDataEntry(Map<String, String> data) throws InterruptedException {
         String[] shortCodeInfo = shortCodeData(data);
 
-        //shortCodePage.clickNewCustomerButton();
+        String storeName = homePage.getStoreName();
+
+        String successText = jsonData.getJSONObject("successMessage").getString("addressValidationSuccessText");
+        String failedText = jsonData.getJSONObject("successMessage").getString("addressValidationFailedText");
+
+        shortCodePage.clickNewCodeButton();
+
+        shortCodePage.enterDeliveryShortCodeDetails(shortCodeInfo[0],shortCodeInfo[1],shortCodeInfo[2],shortCodeInfo[3],shortCodeInfo[4],
+                shortCodeInfo[5],shortCodeInfo[6],shortCodeInfo[7],shortCodeInfo[8],shortCodeInfo[9],storeName,shortCodeInfo[10],
+                shortCodeInfo[11],shortCodeInfo[12],shortCodeInfo[13]);
+
+        shortCodePage.clickValidateButton();
+
+        String validationMessage = shortCodePage.getValidationMessage();
+
+        while (!validationMessage.equalsIgnoreCase(successText)) {
+            if (validationMessage.equalsIgnoreCase(failedText)) {
+                System.out.println("Validation Failed. Retrying...");
+
+                shortCodePage.clickSuggestionText();
+
+                SmallWait(1000);
+                shortCodePage.clickValidateButton();
+            }
+
+            validationMessage = shortCodePage.getValidationMessage();
+        }
+
+        shortCodePage.clickSaveButton();
+
+        shortCodePage.verifyDeliveryShortCodeAddition(shortCodeInfo[0]);
 
         logger.info("Successfully added Delivery Short Code - {}", shortCodeInfo[0]);
     }

862bf4b415129c73e6c8a909c75f879709203515
Fahim59 <mrahaman59@yahoo.com>
2025-04-25 17:16:49 +0600

    POS- Test Case 3 added

--- src/test/java/tests/POS/Test_Case_3.java
+++ src/test/java/tests/POS/Test_Case_3.java
@@ -5,10 +5,8 @@
 package tests.POS;
 
 import base.BaseClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-import pages.Home_Page;
-import pages.OrderEntry_Page;
+import org.testng.annotations.*;
+import pages.*;
 
 import java.io.IOException;
 import java.util.Arrays;

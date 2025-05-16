85663514ea73af0e0a5e6af8a0353cf4a894866b
Fahim59 <mrahaman59@yahoo.com>
2025-05-16 15:55:34 +0600

    delivery slot addition updated

--- src/main/java/pages/Settings/Delivery/DeliveryMode_Page.java
+++ src/main/java/pages/Settings/Delivery/DeliveryMode_Page.java
@@ -3,6 +3,7 @@ package pages.Settings.Delivery;
 import base.BaseClass;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -66,6 +67,119 @@ public class DeliveryMode_Page extends BaseClass{
 
     public void enterDeliveryModeDetails(String name, String... slotOptions) throws InterruptedException {
         enterModeName(name).clickSaveButton().selectDeliverySlot(slotOptions);
+    }
+
+    public void enterDeliveryModeData(String name) throws InterruptedException {
+        enterModeName(name).clickSaveButton();
+    }
+
+    /*
+     * Delivery Slot
+     */
+
+    private final By addDeliverySlotButton = By.xpath("(//span[contains(text(),'Add Delivery Slot')])[1]");
+
+    private final By slotNameField = By.xpath("//input[@formcontrolname='name']");
+
+    private final By slotSaveBtn = By.xpath("(//span[contains(text(),'Save')])[2]");
+
+    private final String slotTable = "//table[@role='table']/tbody";
+    private final By rows = By.xpath(slotTable+"/tr");
+
+    private final By zoneField = By.xpath("(//div[contains(@id,'mat-select-value')])[3]");
+    private final By slotTimeField = By.xpath("//input[@placeholder='HH:MM']");
+    private final By amPmField = By.xpath("(//div[contains(@id,'mat-select-value')])[4]");
+
+    public DeliveryMode_Page clickAddSlotButton() throws InterruptedException {
+        SmallWait(1000);
+        click_Element(addDeliverySlotButton);
+
+        SmallWait(2000);
+
+        return this;
+    }
+
+    public DeliveryMode_Page enterSlotName(String name) throws InterruptedException {
+        SmallWait(200);
+        write_Send_Keys(slotNameField, name);
+        return this;
+    }
+
+    public DeliveryMode_Page clickSlotSaveButton() throws InterruptedException {
+        SmallWait(1000);
+        click_Element(slotSaveBtn);
+
+        SmallWait(2000);
+
+        return this;
+    }
+
+    public void selectDeliveryZone(String... zoneOptions) throws InterruptedException {
+        click_Element(zoneField);
+        SmallWait(200);
+
+        try {
+            for (String option : zoneOptions) {
+                WebElement checkbox = driver.findElement(By.xpath("//span[text()=concat(' ', '"+option+"', ' ')]"));
+
+                if (checkbox == null) {
+                    throw new Exception("Invalid zone option: " +option);
+                }
+
+                if (!checkbox.isSelected()) {
+                    js.executeScript("arguments[0].click();", checkbox);
+                }
+            }
+            actions.sendKeys(Keys.TAB).perform();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void enterSlotTime(String hours, String minute) {
+        String time = hours + ":" + minute;
+        write_Send_Keys(slotTimeField, time);
+    }
+    public void selectAmPm(String text) throws InterruptedException {
+        if(!get_Text(amPmField).equals(text)){
+            click_Element(amPmField);
+            SmallWait(200);
+            js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//span[normalize-space()='"+text+"']")));
+        }
+    }
+
+    public void enterSlotDetails(String slotName, String hours, String minute, String text, String... zoneOptions) throws InterruptedException {
+        SmallWait(2000);
+        for(int l = 1; l<= get_Size(rows); l++){
+
+            String name = driver.findElement(By.xpath(slotTable+ "/tr["+l+"]/td[1]")).getText();
+            WebElement actionButton = driver.findElement(By.xpath("(//span[contains(text(),'Action')])["+l+"]"));
+
+            if(name.equalsIgnoreCase(slotName)){
+                actionButton.click();
+
+                WebElement configButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[contains(text(),'Configure')])")));
+                configButton.click();
+
+                SmallWait(1000);
+
+                selectDeliveryZone(zoneOptions);
+                enterSlotTime(hours, minute);
+                selectAmPm(text);
+                clickSaveButton();
+
+                logger.info("Slot {} found", slotName);
+                break;
+            }
+            else{
+                System.out.println("Slot Not Found");
+            }
+        }
+    }
+
+    public void enterDeliverySlotData(String name) throws InterruptedException {
+        clickAddSlotButton().enterSlotName(name).clickSlotSaveButton();
     }
 
     /*
--- src/test/java/tests/Settings/Delivery/Delivery_Mode.java
+++ src/test/java/tests/Settings/Delivery/Delivery_Mode.java
@@ -33,9 +33,13 @@ public class Delivery_Mode extends BaseClass {
     public static String[] modeData(Map<String, String> valueData) {
         String name = valueData.get("Mode Name");
         String slot1 = valueData.get("Slot1");
-        String slot2 = valueData.get("Slot2");
+        String zone1 = valueData.get("Zone1");
+        String zone2 = valueData.get("Zone2");
+        String hour = valueData.get("Hour");
+        String minute = valueData.get("Minute");
+        String amOrPm = valueData.get("AM/PM");
 
-        return new String[] {name, slot1, slot2};
+        return new String[] {name, slot1, hour, minute, amOrPm, zone1, zone2};
     }
 
     @Test(description = "Verify that the user can add Delivery Mode data successfully", dataProvider = "excelData", dataProviderClass = DataSource.class, priority = 2)
@@ -45,7 +49,11 @@ public class Delivery_Mode extends BaseClass {
 
         deliveryModePage.clickNewModeButton();
 
-        deliveryModePage.enterDeliveryModeDetails(modeInfo[0], modeInfo[1], modeInfo[2]);
+        deliveryModePage.enterDeliveryModeData(modeInfo[0]);
+
+        deliveryModePage.enterDeliverySlotData(modeInfo[1]);
+
+        deliveryModePage.enterSlotDetails(modeInfo[1], modeInfo[2], modeInfo[3], modeInfo[4], modeInfo[5], modeInfo[6]);
 
         deliveryModePage.clickFinalSaveButton();
 
